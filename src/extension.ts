8cc457d85470543d32c7b8853d91adaa0fba721e
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2023-09-26 18:07:45 +0200

    beggining

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { SidebarProvider } from './SidebarProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,7 +10,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "turbocmd" is now active!');
-
+	  const provide=new SidebarProvider(context.extensionUri);
+	  context.subscriptions.push(vscode.window.registerWebviewViewProvider('vstodo-sidebar',provide))	
+	 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
